11ab64900195e30db56a68897fa7f9acc33450ff
Lan Anh <nguyenlananh2906@gmail.com>
2026-06-29 10:44:19 +0700

    video 21

---
+++ src/redux/counter/counter.slice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface CounterState {
+    value: number
+}
+
+const initialState = {
+    value: 0,
+    name: 'lan anh',
+    address: {
+        name: "ha noi",
+        code: 100
+    }
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+            state.value += 1
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, } = counterSlice.actions
+
+export default counterSlice.reducer
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import counterReducer from './counter/counter.slice'
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        count: counterReducer
+    },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
